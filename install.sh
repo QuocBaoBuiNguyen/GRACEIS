1cfee54cd2038eb4f0625f0f93cc764a1ba29adc
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2025-09-20 22:58:25 +0700

    fix: update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
-import torch
+#!/bin/bash
 
-!pip install torch-scatter -f https://data.pyg.org/whl/torch-{torch.__version__}.html
-!pip install torch-sparse -f https://data.pyg.org/whl/torch-{torch.__version__}.html
-!pip install torch-cluster -f https://data.pyg.org/whl/torch-{torch.__version__}.html
-!pip install git+https://github.com/pyg-team/pytorch_geometric.git
-!pip install torch-spline-conv -f https://data.pyg.org/whl/torch-{torch.__version__}.html
+# Install necessary libraries
+pip install torch-scatter -f https://data.pyg.org/whl/torch-$(python -c 'import torch; print(torch.__version__)').html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-$(python -c 'import torch; print(torch.__version__)').html
+pip install torch-cluster -f https://data.pyg.org/whl/torch-$(python -c 'import torch; print(torch.__version__)').html
+pip install git+https://github.com/pyg-team/pytorch_geometric.git
+pip install torch-spline-conv -f https://data.pyg.org/whl/torch-$(python -c 'import torch; print(torch.__version__)').html
 
 # Additional required packages
-!pip install torch-geometric >= 1.4.3
-!pip install scikit-learn
-!pip install numpy
-!pip install pyyaml
-!pip install ogb
-!pip install wandb
-!pip install matplotlib
+pip install torch-geometric >= 1.4.3
+pip install scikit-learn
+pip install numpy
+pip install pyyaml
+pip install ogb
+pip install wandb
+pip install matplotlib
